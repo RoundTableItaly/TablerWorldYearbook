a6df89f349209d9ef2a8e2963e1c14f69b52bc1e
Riccardo Dal Fiume <riccardo@dalfiume.io>
2023-07-29 16:54:45 +0200

    Data cleansing
    Clean first_name, last_name, phonenumbers, address

--- main.py
+++ main.py
@@ -19,7 +19,44 @@ def main():
             contacts = json.load(file_contacts)
 
     df = pd.DataFrame(contacts)
-    print(df)
+
+    # Clean rows
+    def clean_name(cell):
+        return cell.title()
+
+    def clean_phonenumbers(cell):
+        if not cell:
+            return "-"
+
+        mobile = None
+        home = None
+        for item in cell:
+            if item.get("type", None) == "home":
+                home = item.get("value", None)
+            if item.get("type", None) == "mobile":
+                mobile = item.get("value", None)
+
+        return mobile if not None else home
+
+    def clean_address(cell):
+        if not cell:
+            return "-"
+
+        item = cell[0]
+
+        street1 = item.get("street1", None)
+        street2 = item.get("street2", None)
+        postal_code = item.get("postal_code", None)
+        city = item.get("city", None)
+
+        return f"{street1} - {postal_code} {city}"
+
+    df["first_name"] = df["first_name"].apply(clean_name)
+    df["last_name"] = df["last_name"].apply(clean_name)
+    df["phonenumbers"] = df["phonenumbers"].apply(clean_phonenumbers)
+    df["address"] = df["address"].apply(clean_address)
+
+    # Save excel file
     df.to_excel(FILE_CONTACTS_EXCEL, sheet_name="contacts", index=False)
 
 
