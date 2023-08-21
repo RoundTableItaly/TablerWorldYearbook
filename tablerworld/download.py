824bc00cab24b4fef490c37b27179693d7f049cb
Riccardo Dal Fiume <riccardo@dalfiume.io>
2023-08-21 19:36:23 +0200

    ADD dowload profile_pic

--- tablerworld/download.py
+++ tablerworld/download.py
@@ -2,6 +2,11 @@ import os
 import requests
 import logging
 from dotenv import load_dotenv
+import pandas as pd
+import threading
+import queue
+import requests
+import traceback
 
 
 def contacts():
@@ -50,3 +55,47 @@ def contacts():
         print(f"request {index} - records {records} - status {page.status_code}")
 
     return results
+
+
+def profile_pictures(df):
+    df_pictures = df.dropna(subset=["profile_pic"])
+
+    q = queue.Queue()
+
+    def worker():
+        # print(f"Started thread: {threading.current_thread().name}")
+        while True:
+            try:
+                item = q.get()
+                id = f"rt{item['rt_club_number']:02} - {item['last_name']} {item['first_name']}"
+                print(f"Started  {id}")
+
+                r = requests.get(item["profile_pic"])
+                with open(f"dist/profile_pics/{id}.jpg", "wb") as f:
+                    f.write(r.content)
+
+            except Exception:
+                print(traceback.format_exc())
+                while not q.empty():
+                    try:
+                        q.get(block=False)
+                    except Empty:
+                        continue
+                    q.task_done()
+
+            print(f"Finished {id}")
+            q.task_done()
+
+    # Turn-on the worker threads
+    for i in range(10):
+        threading.Thread(target=worker, daemon=True).start()
+
+    # Send thirty task requests to the worker.
+    for index, item in df_pictures.iterrows():
+        q.put(item)
+
+    # Block until all tasks are done.
+    q.join()
+    print("All work completed")
+
+    return
