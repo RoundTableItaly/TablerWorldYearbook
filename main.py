a42ad38e0a1e6ae990fc5c229f64e55be7dbed38
Riccardo Dal Fiume <riccardo@dalfiume.io>
2023-10-12 16:02:40 +0200

    FIX rt_global_positions_national
    Improve data merge with manual_contacts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tablerworld.download
 import tablerworld.contacts
 import tablerworld.report
-from pathlib import Path, PurePath
+from pathlib import Path
 from dotenv import load_dotenv
 import logging
 import json
@@ -9,11 +9,11 @@ import os
 import pandas as pd
 
 MODULE_PATH = Path(__file__).parent
-DIST_FOLDER = PurePath.joinpath(Path(MODULE_PATH), "dist")
+DIST_FOLDER = Path(MODULE_PATH, "dist")
 
-FILE_CONTACTS_JSON = PurePath.joinpath(DIST_FOLDER, "data_contacts.json")
-FILE_CONTACTS_EXCEL = PurePath.joinpath(DIST_FOLDER, "data_contacts.xlsx")
-FILE_CONTACTS_EXCEL_DIRTY = PurePath.joinpath(DIST_FOLDER, "data_contacts_dirty.xlsx")
+FILE_CONTACTS_JSON = Path(DIST_FOLDER, "data_contacts.json")
+FILE_CONTACTS_EXCEL = Path(DIST_FOLDER, "data_contacts.xlsx")
+FILE_CONTACTS_EXCEL_DIRTY = Path(DIST_FOLDER, "data_contacts_dirty.xlsx")
 
 FILE_MANUAL_CONTACTS_EXCEL = "manual_contacts.xlsx"
 
@@ -21,53 +21,36 @@ FILE_MANUAL_CONTACTS_EXCEL = "manual_contacts.xlsx"
 def main():
     load_dotenv()
 
-    logger = logging.getLogger()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.WARN)
+    logging.basicConfig(level=logging.WARN, format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 
-    DOWNLOAD_CONTACTS = True
+    DOWNLOAD_CONTACTS = False
     CONTACTS_CLEAN = True
     CONTACTS_EXPORT_XLSX = True
-    DOWNLOAD_PROFILE_PICTURES = True
+    DOWNLOAD_PROFILE_PICTURES = False
     GENERATE_REPORT = True
 
-    # FILE_CONTACTS
     if not os.path.isfile(FILE_CONTACTS_JSON) or DOWNLOAD_CONTACTS:
-        # Download contacts if not available or if forced
         contacts = tablerworld.download.contacts()
         with open(FILE_CONTACTS_JSON, "w") as file_contacts:
             json.dump(contacts, file_contacts)
     else:
-        # Read and parse contacts file
         with open(FILE_CONTACTS_JSON, "r") as file_contacts:
             contacts = json.load(file_contacts)
 
-    # FILE_MANUAL_CONTACTS
     if os.path.isfile(FILE_MANUAL_CONTACTS_EXCEL):
         df_manual_contacts = pd.read_excel(FILE_MANUAL_CONTACTS_EXCEL)
 
-    # Create Pandas dataframe
     df = pd.DataFrame(contacts)
 
-    # Save excel file DIRTY
-    # df.to_excel(FILE_CONTACTS_EXCEL_DIRTY, sheet_name="contacts", index=False)
-
-    # Apply cleaning
     if CONTACTS_CLEAN:
         df = tablerworld.contacts.clean(df, df_manual_contacts)
 
-    # Save excel file
     if CONTACTS_EXPORT_XLSX:
         df.to_excel(FILE_CONTACTS_EXCEL, sheet_name="contacts", index=False)
 
-    # Download profile pictures
     if DOWNLOAD_PROFILE_PICTURES:
         tablerworld.download.profile_pictures(df)
 
-    # Generate report
     if GENERATE_REPORT:
         tablerworld.report.report(df)
 
